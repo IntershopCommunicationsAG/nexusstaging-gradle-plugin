55ad516743ea598d30cab6ca0ad6ef68458b027e
mraab <mraab@intershop.de>
2016-09-10 10:41:09 +0200

    add build parameter (test runs localy but not on the remote server)

--- buildTravis.sh
+++ buildTravis.sh
@@ -7,7 +7,7 @@ export GRADLE_OPTS="-Dorg.gradle.daemon=true"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
-        ./gradlew test build -s
+        ./gradlew test build -s -i
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] Tag ['$TRAVIS_TAG']'
         ./gradlew -PrunOnCI=true test build :bintrayUpload :publishPlugins -s
@@ -15,7 +15,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 else
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
-        ./gradlew test build -s
+        ./gradlew test build -s -i
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] Tag ['$TRAVIS_TAG']'
         ./gradlew -PrunOnCI=true test build :bintrayUpload :publishPlugins -s
